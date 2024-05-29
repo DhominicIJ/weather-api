4848ff5fa277946c5cc404d5dec170c00760bc66
DhominicIJ <164959939+DhominicIJ@users.noreply.github.com>
2024-05-28 21:54:25 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
 // aa5764616697886870b3e2a5e28b45ea
+
+//api bandeira//
+const data = await getWeatherData(city);
+
+const apiCountryURL = `https://flagsapi.com/${data.sys.country}/flat/64.png`;
+
+cityElement.innerText = data.name;
+//api bandeira//
